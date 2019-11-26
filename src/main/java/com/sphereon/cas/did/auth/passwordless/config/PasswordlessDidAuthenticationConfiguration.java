13ec52a7c626aab31fdb45965a533c033343cc8a
Scott Malley <srmalley-sphereon@github.com>
2019-11-26 10:57:10 +0100

    update configuration to mimic default passwordless

--- src/main/java/com/sphereon/cas/did/auth/passwordless/config/PasswordlessDidAuthenticationConfiguration.java
+++ src/main/java/com/sphereon/cas/did/auth/passwordless/config/PasswordlessDidAuthenticationConfiguration.java
@@ -7,6 +7,7 @@ import com.sphereon.cas.did.auth.passwordless.token.DidTokenRepository;
 import com.sphereon.cas.did.auth.passwordless.token.InMemoryDidTokenRepository;
 import com.sphereon.cas.did.auth.passwordless.web.flow.AcceptPasswordlessDidAuthenticationAction;
 import com.sphereon.cas.did.auth.passwordless.web.flow.DisplayBeforePasswordlessDidAuthentication;
+import com.sphereon.cas.did.auth.passwordless.web.flow.PasswordlessDidAuthenticationWebflowConfigurer;
 import com.sphereon.cas.did.auth.passwordless.web.flow.PrepareForPasswordlessDidAuthentication;
 import com.sphereon.cas.did.auth.passwordless.web.flow.VerifyPasswordlessDidAuthenticationAction;
 import com.sphereon.libs.did.auth.client.DidAuthFlow;
@@ -25,6 +26,9 @@ import org.apereo.cas.authentication.principal.PrincipalResolver;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.support.passwordless.PasswordlessAuthenticationProperties;
 import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.web.flow.CasWebflowConfigurer;
+import org.apereo.cas.web.flow.CasWebflowExecutionPlan;
+import org.apereo.cas.web.flow.CasWebflowExecutionPlanConfigurer;
 import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
 import org.springframework.beans.factory.ObjectProvider;
@@ -33,15 +37,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 import org.springframework.webflow.execution.Action;
 
 import java.net.http.HttpClient;
 
 @Configuration("passwordlessAuthenticationConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
-public class PasswordlessDidAuthenticationConfiguration {
+public class PasswordlessDidAuthenticationConfiguration implements CasWebflowExecutionPlanConfigurer {
 
     private final int didMapPort;
     private final String didTransportsUrl;
@@ -57,6 +65,9 @@ public class PasswordlessDidAuthenticationConfiguration {
     private final ObjectProvider<AuthenticationSystemSupport> authenticationSystemSupport;
     private final ObjectProvider<ServicesManager> servicesManager;
     private final ObjectProvider<PrincipalResolver> defaultPrincipalResolver;
+    private final ApplicationContext applicationContext;
+    private final FlowBuilderServices flowBuilderServices;
+    private final ObjectProvider<FlowDefinitionRegistry> loginFlowDefinitionRegistry;
 
     public PasswordlessDidAuthenticationConfiguration(@Value("${sphereon.cas.did.auth.appId}") String appId,
                                                       CasConfigurationProperties casProperties,
@@ -70,7 +81,10 @@ public class PasswordlessDidAuthenticationConfiguration {
                                                       @Value("${sphereon.cas.did.auth.didMapHost}") final String didMapHost,
                                                       @Value("${sphereon.cas.did.auth.didTransportsUrl}") final String didTransportsUrl,
                                                       @Value("${sphereon.cas.did.auth.appDid}") final String appDid,
-                                                      @Value("${sphereon.cas.did.auth.appSecret}") final String appSecret) {
+                                                      @Value("${sphereon.cas.did.auth.appSecret}") final String appSecret,
+                                                      ApplicationContext applicationContext,
+                                                      FlowBuilderServices flowBuilderServices,
+                                                      @Qualifier("loginFlowRegistry") ObjectProvider<FlowDefinitionRegistry> loginFlowDefinitionRegistry) {
         this.didMapPort = didMapPort;
         this.didMapHost = didMapHost;
         this.didTransportsUrl = didTransportsUrl;
@@ -84,6 +98,9 @@ public class PasswordlessDidAuthenticationConfiguration {
         this.authenticationSystemSupport = authenticationSystemSupport;
         this.servicesManager = servicesManager;
         this.defaultPrincipalResolver = defaultPrincipalResolver;
+        this.applicationContext = applicationContext;
+        this.flowBuilderServices = flowBuilderServices;
+        this.loginFlowDefinitionRegistry = loginFlowDefinitionRegistry;
     }
 
     /*
@@ -193,5 +210,18 @@ public class PasswordlessDidAuthenticationConfiguration {
     public AuthenticationEventExecutionPlanConfigurer passwordlessAuthenticationEventExecutionPlanConfigurer() {
         return plan -> plan.registerAuthenticationHandlerWithPrincipalResolver(
                 passwordlessTokenAuthenticationHandler(), defaultPrincipalResolver.getObject());
+    }
+
+    @ConditionalOnMissingBean(name = "passwordlessAuthenticationWebflowConfigurer")
+    @Bean
+    @DependsOn("defaultWebflowConfigurer")
+    public CasWebflowConfigurer passwordlessAuthenticationWebflowConfigurer() {
+        return new PasswordlessDidAuthenticationWebflowConfigurer(flowBuilderServices,
+                loginFlowDefinitionRegistry.getIfAvailable(), applicationContext, casProperties);
+    }
+
+    @Override
+    public void configureWebflowExecutionPlan(final CasWebflowExecutionPlan plan) {
+        plan.registerWebflowConfigurer(passwordlessAuthenticationWebflowConfigurer());
     }
 }
