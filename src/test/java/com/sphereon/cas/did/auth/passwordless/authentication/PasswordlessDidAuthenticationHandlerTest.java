e01abafe38b1d4d20a627f7a5158e39fbeb05398
Scott Malley <srmalley-sphereon@github.com>
2019-12-05 13:20:41 +0100

    updated test

--- src/test/java/com/sphereon/cas/did/auth/passwordless/authentication/PasswordlessDidAuthenticationHandlerTest.java
+++ src/test/java/com/sphereon/cas/did/auth/passwordless/authentication/PasswordlessDidAuthenticationHandlerTest.java
@@ -3,6 +3,7 @@ package com.sphereon.cas.did.auth.passwordless.authentication;
 import com.sphereon.cas.did.auth.passwordless.token.DidToken;
 import com.sphereon.cas.did.auth.passwordless.token.InMemoryDidTokenRepository;
 import com.sphereon.libs.did.auth.client.DidAuthFlow;
+import com.sphereon.libs.did.auth.client.exceptions.MalformedLoginJwtException;
 import org.apereo.cas.authentication.PreventedException;
 import org.apereo.cas.authentication.credential.OneTimePasswordCredential;
 import org.apereo.cas.authentication.principal.PrincipalFactoryUtils;
@@ -10,6 +11,7 @@ import org.apereo.cas.services.ServicesManager;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.security.auth.login.FailedLoginException;
 import java.security.GeneralSecurityException;
 
 import static org.junit.Assert.assertNotNull;
@@ -31,12 +33,23 @@ public class PasswordlessDidAuthenticationHandlerTest {
     }
 
     @Test
-    public void verifyAction() throws GeneralSecurityException, PreventedException {
+    public void authenticateShouldVerifyWhenDidAuthFlowVerifies() throws GeneralSecurityException, PreventedException {
         var repository = new InMemoryDidTokenRepository(60);
         var token = new DidToken("testRequestToken");
         repository.saveToken("testUser", token.with("testResponseToken"));
-        var c = new OneTimePasswordCredential("testUser", "responseToken");
+        var c = new OneTimePasswordCredential("testUser", "testResponseToken");
         when(didAuthFlow.verifyLoginToken("testResponseToken")).thenReturn("testResponseToken");
         assertNotNull(passwordlessDidAuthenticationHandler.authenticate(c));
+    }
+
+    @Test(expected = FailedLoginException.class)
+    public void authenticateShouldNotVerifyWhenDidAuthThrowsError() throws GeneralSecurityException, PreventedException {
+        var repository = new InMemoryDidTokenRepository(60);
+        var token = new DidToken("testRequestToken");
+        repository.saveToken("testUser", token.with("testResponseToken"));
+        var c = new OneTimePasswordCredential("testUser", "testResponseToken");
+        when(didAuthFlow.verifyLoginToken("testResponseToken"))
+                .thenThrow(new MalformedLoginJwtException("JWT does not contain correct signature"));
+        passwordlessDidAuthenticationHandler.authenticate(c);
     }
 }
